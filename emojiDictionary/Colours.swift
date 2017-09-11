78ba032e28f8ffffe7c0a8226a6719b950229b55
Ryan Morrison <30383652+egodevrjm@users.noreply.github.com>
2017-09-11 21:25:26 +0100

    Added info

---
+++ emojiDictionary/Colours.swift
@@ -0,0 +1,12 @@
+//
+//  Colours.swift
+//  emojiMeaning
+//
+//  Created by Ryan Morrison on 11/09/2017.
+//  Copyright © 2017 egoDev. All rights reserved.
+//
+
+import UIKit
+
+let lemon = UIColor(displayP3Red: 255/255, green: 255/255, blue: 0/255, alpha: 1.0)
+let dBlue = UIColor(displayP3Red: 41/255, green: 128/255, blue: 185/255, alpha: 1.0)
--- emojiDictionary/MainTableViewController.swift
+++ emojiDictionary/MainTableViewController.swift
@@ -13,17 +13,41 @@ class MainTableViewController: UITableViewController {
     // Variables
     
     var emojiArray : [Emoji] = []
+    let searchController = UISearchController(searchResultsController: nil)
+    var filteredEmoji = [Emoji]()
     
-   
     @IBOutlet weak var searchBar: UISearchBar!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         emojiArray = Emoji().createEmoji()
+        filteredEmoji = emojiArray
+        
+        // Setup the Search Controller
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
+        
     }
 
-   
+    func searchBarIsEmpty() -> Bool {
+        // Returns true if the text is empty or nil
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        filteredEmoji = emojiArray.filter({( emojiArray : Emoji) -> Bool in
+            return emojiArray.emojiTitle.lowercased().contains(searchText.lowercased())
+        })
+        
+        tableView.reloadData()
+    }
+    
+    func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
+    }
 
     // MARK: - Table view data source
 
@@ -33,23 +57,41 @@ class MainTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if isFiltering() {
+            return filteredEmoji.count
+        }
+        
         return emojiArray.count
+        
+        
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        let emoji = emojiArray[indexPath.row]
+        let emoji: Emoji
+        if isFiltering() {
+            emoji = filteredEmoji[indexPath.row]
+        } else {
+            emoji = emojiArray[indexPath.row]
+        }
         
         cell.textLabel?.text = emoji.emojiView
         cell.detailTextLabel?.text = emoji.emojiTitle
         
+        
         return cell
     }
     
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let emoji = emojiArray[indexPath.row]
+        let emoji: Emoji
+        if isFiltering() {
+            emoji = filteredEmoji[indexPath.row]
+        } else {
+            emoji = emojiArray[indexPath.row]
+        }
         performSegue(withIdentifier: "detail", sender: emoji)
     }
     
@@ -59,7 +101,13 @@ class MainTableViewController: UITableViewController {
         emojiVC.emoji = sender as! Emoji
        
     }
-    
-   
+}
 
+
+
+extension MainTableViewController: UISearchResultsUpdating {
+    // MARK: - UISearchResultsUpdating Delegate
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
 }
